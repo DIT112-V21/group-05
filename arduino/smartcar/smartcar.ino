8e81fcc93eb0ce2a4bb593c2eab1cca2c3bd58e7
AlbertSimpson00 <71037016+AlbertSimpson00@users.noreply.github.com>
2021-04-08 17:01:04 +0200

    Update smartcar.ino
    basic obstacle avoidance, stops the car if reaches within 20cm of an object

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,48 @@
 #include <Smartcar.h>
 
+const int TRIGGER_PIN               = 6;
+const int ECHO_PIN                  = 7;
+const unsigned int MAX_DISTANCE     = 100;
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+SimpleCar car(control);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  handleInput();
+  stop();
+}
+
+void handleInput()
+{
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        if (input.startsWith("m"))
+        {
+            int throttle = input.substring(1).toInt();
+            car.setSpeed(throttle);
+        }
+        else if (input.startsWith("t"))
+        {
+            int deg = input.substring(1).toInt();
+            car.setAngle(deg);
+        }
+    }
+}
+
+void stop() {
+    if(front.getDistance() > 20) {
+      car.setSpeed(0);
+      delay(500);
+    }
 }
