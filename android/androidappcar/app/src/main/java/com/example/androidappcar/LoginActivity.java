6668c7afe5dbf685550999a0fcc002a84988bc4b
Enes Akin <denesakin@gmail.com>
2021-05-08 20:11:44 +0200

    Login/register pages
    backend is also working rn, however i will make changes to the frontend, this is more of a "sketch"

---
+++ android/androidappcar/app/src/main/java/com/example/androidappcar/LoginActivity.java
@@ -0,0 +1,116 @@
+package com.example.androidappcar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
+
+    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore  fStore = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        gotoRegister = findViewById(R.id.gotoRegister);
+
+        loginBtn.setOnClickListener(v -> {
+            checkField(email);
+            checkField(password);
+
+            if (valid) {
+                fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(authResult -> {
+                    Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                    checkUserAccessLevel(authResult.getUser().getUid());
+                }).addOnFailureListener(e -> {
+                    Toast.makeText(LoginActivity.this, "Failed to log in", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+
+        gotoRegister.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
+    }
+
+    private void checkUserAccessLevel(String uid) {
+        DocumentReference df = fStore.collection("Users").document(uid);
+
+        df.get().addOnSuccessListener(documentSnapshot -> {
+            Log.d("TAG", "onSuccess" + documentSnapshot.getData());
+
+            if (documentSnapshot.getString("isStaff") != null) {
+                // (STAFF) these activities will be changes later based on the users type
+                startActivity(new Intent(getApplicationContext(), TestActivity.class));
+                finish();
+            } else {
+                // (PATIENT) these activities will be changes later based on the users type
+                startActivity(new Intent(getApplicationContext(), TestActivity.class));
+                finish();
+            }
+        });
+    }
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (fAuth.getCurrentUser() != null) {
+            DocumentReference df = fStore.collection("Users").document(fAuth.getCurrentUser().getUid());
+
+            df.get().addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.getString("isStaff") != null) {
+                    Toast.makeText(LoginActivity.this, "Welcome back " + documentSnapshot.getString("FullName"), Toast.LENGTH_SHORT).show();
+
+                    // (STAFF) these activities will be changes later based on the users type
+                    startActivity(new Intent(getApplicationContext(), TestActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Welcome back " + documentSnapshot.getString("FullName"), Toast.LENGTH_SHORT).show();
+
+                    // (PATIENT) these activities will be changes later based on the users type
+                    startActivity(new Intent(getApplicationContext(), TestActivity.class));
+                    finish();
+                }
+            }).addOnFailureListener(e -> {
+                fAuth.signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            });
+        }
+    }
+}
